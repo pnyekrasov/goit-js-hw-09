68ef1f1c86a89cea1b10aa6234fe70cfab0fb893
pnyekrasov <pnekrasov1910@gmail.com>
2023-06-26 12:44:54 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,17 +7,20 @@ const backgroundColorBodyEl = document.querySelector('body');
 btnStartEl.addEventListener('click', () => {
     btnStartEl.disabled = true;
     btnStopEl.disabled = false;
-      timerIntervalId = setInterval(() => {
+    timerIntervalId = setInterval(() => {
            backgroundColorBodyEl.style.backgroundColor = getRandomHexColor();
   }, 1000)        
 });
 
 btnStopEl.addEventListener('click', () => {
-    clearInterval(timerIntervalId);
-    btnStartEl.disabled = false;
-    btnStopEl.disabled = true;    
+      btnStartEl.disabled = false;
+    btnStopEl.disabled = true; 
+  clearInterval(timerIntervalId);
+  
+   
 });
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+
