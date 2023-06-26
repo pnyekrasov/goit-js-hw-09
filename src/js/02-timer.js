22db88437f48d771f4ccee345697487ba3cd817a
pnyekrasov <pnekrasov1910@gmail.com>
2023-06-26 18:35:30 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,16 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+
+const inputStartDataEl = document.querySelector('#datetime-picker');
+
+const btnStartDataEl = document.querySelector('button[data-start]');
+
+flatpickr(inputStartDataEl, {
+enableTime: true,
+time_24hr: true,
+defaultDate: new Date(),
+minuteIncrement: 1,
+onClose(selectedDates) {
+console.log(selectedDates[0]);
+},
+});
